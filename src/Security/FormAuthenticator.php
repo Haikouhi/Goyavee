7d7f5a651b2b63a7e18b1bdf87f534e18d800ac2
wnslwlch <barthelemycourty@gmail.com>
2019-02-26 10:14:26 +0100

    fix(formauthenticator): add a redirection after registering

--- src/Security/FormAuthenticator.php
+++ src/Security/FormAuthenticator.php
@@ -86,7 +86,10 @@ class FormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+
+        return new RedirectResponse($this->urlGenerator->generate('user_index'));
+
     }
 
     protected function getLoginUrl()

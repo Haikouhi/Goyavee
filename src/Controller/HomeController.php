bb0ac13e795d4d2f107557b052cd93bed8fc7a5b
CarolineChap <caroline2chapeau@gmail.com>
2019-03-01 11:35:02 +0100

    register path and content done : birthdate input to fix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     /**

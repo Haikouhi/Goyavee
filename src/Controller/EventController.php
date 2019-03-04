f6d8929c08f2afa00237a28393ffccb27ef0d36a
wnslwlch <barthelemycourty@gmail.com>
2019-03-04 11:28:01 +0100

    fix(event, marketting): add isgranted use in both files

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\CommentType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @IsGranted("ROLE_USER")
